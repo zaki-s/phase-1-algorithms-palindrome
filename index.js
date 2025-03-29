e88dc0aa52fe856ff0578556fae01866d73b280d
zaki-s <zakijames024@gmail.com>
2025-03-29 23:51:11 +0300

    G

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ function isPalindrome(word) {
   // Write your algorithm here
 }
 
+
+// Function to check if a string is a palindrome
+function isPalindrome(str) {
+  // Reverse the string and compare it to the original
+  return str === str.split('').reverse().join('');
+}
+
+// Test cases
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("robot")); // false
+console.log(isPalindrome("hello")); // false
+
+// Export function for testing
+module.exports = isPalindrome;
+
 /* 
   Add your pseudocode here
 */
